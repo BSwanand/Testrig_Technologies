a9438fb52b26799ed2fc118a23db11287e4f0d6a
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2022-02-11 18:02:58 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,52 +1,34 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture
-def setup(browser,autouse=True):
-    if browser=='chrome':
-        driver=webdriver.Chrome()
+
+@pytest.fixture()
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome(r"C:\Users\welcome\Desktop\chromedriver.exe")
         print("Launching chrome browser.........")
-    elif browser=='firefox':
-        driver = webdriver.Firefox()
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(executable_path=r"C:\Users\welcome\Desktop\geckodriver.exe")
         print("Launching firefox browser.........")
-
-    else:
-        driver=webdriver.Ie()
-
     return driver
 
 
-
-def pytest_addoption(parser):    # This will get the value from CLI /hooks
+def pytest_addoption(parser):  # This will get the value from CLI /hooks
     parser.addoption("--browser")
+
 
 @pytest.fixture()
 def browser(request):  # This will return the Browser value to setup method
     return request.config.getoption("--browser")
 
-#
-###pytest html report####
+# It is hook for Adding Environment info to HTML Report
 def pytest_configure(config):
-    config._metadata['Project name']='nop Commerce'
-    config._metadata['Module Name']='Customers'
-    config._metadata['Tester']='Sahil'
-####it is hook for delete modify enviroment
+    config._metadata['Project Name'] = 'nop Commerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Swanand'
+
+# It is hook for delete/Modify Environment info to HTML Report
 @pytest.mark.optionalhook
 def pytest_metadata(metadata):
-    metadata.pop("JAVA_HOME",None)
-    metadata.pop("Plugins",None)
-
-#
-# # It is hook for Adding Environment info to HTML Report
-# def pytest_configure(config):
-#     config._metadata['Project Name'] = 'nop Commerce'
-#     config._metadata['Module Name'] = 'Customers'
-#     config._metadata['Tester'] = 'Pavan'
-#
-# # It is hook for delete/Modify Environment info to HTML Report
-# @pytest.mark.optionalhook
-# def pytest_metadata(metadata):
-#     metadata.pop("JAVA_HOME", None)
-#     metadata.pop("Plugins", None)
-
-
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
