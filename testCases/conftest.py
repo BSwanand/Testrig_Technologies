8c751a9bd22a7687d95a45e557df47a0e6d38777
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2022-02-14 11:29:26 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,15 +1,23 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 
 
 @pytest.fixture()
 def setup(browser):
     if browser == 'chrome':
-        driver = webdriver.Chrome(r"C:\Users\welcome\Desktop\chromedriver.exe")
+        # driver = webdriver.Chrome(r"C:\Users\welcome\Desktop\chromedriver.exe")
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        return driver
         print("Launching chrome browser.........")
     elif browser == 'firefox':
-        driver = webdriver.Firefox(executable_path=r"C:\Users\welcome\Desktop\geckodriver.exe")
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         print("Launching firefox browser.........")
+
+
     return driver
 
 
@@ -25,7 +33,7 @@ def browser(request):  # This will return the Browser value to setup method
 def pytest_configure(config):
     config._metadata['Project Name'] = 'nop Commerce'
     config._metadata['Module Name'] = 'Customers'
-    config._metadata['Tester'] = 'Swanand'
+    config._metadata['Tester'] = 'Sahil'
 
 # It is hook for delete/Modify Environment info to HTML Report
 @pytest.mark.optionalhook
